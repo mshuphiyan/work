5c6e9c6f4a8d8f6529fbb19da56ac9e4b4549489
Md Shuphiyan <md.shuphiyan@gmail.com>
2020-08-24 23:38:51 +0530

    add form

--- src/app/modules/device/devices/adddevice/adddevice.component.ts
+++ src/app/modules/device/devices/adddevice/adddevice.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-adddevice',
@@ -7,12 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdddeviceComponent implements OnInit {
 
-  constructor() { }
+  // addNewDevice: FormGroup;
+  // deviceStatus = new FormControl(false);
+
+  constructor(fb: FormBuilder) {
+    // this.addNewDevice = fb.group({
+    //   deviceStatus: this.deviceStatus,
+    // });
+  }
+  addNewDevice = new FormGroup({
+    deviceName: new FormControl(''),
+    deviceModel: new FormControl(''),
+    manufacturer: new FormControl(''),
+    deviceType: new FormControl(''),
+    description: new FormControl(''),
+    deviceStatus: new FormControl(''),
+    portNumber: new FormControl(''),
+    ipAddress: new FormControl(''),
+    location: new FormControl(''),
+    driverManager: new FormControl(''),
+    driverName: new FormControl(''),
+    connectionType: new FormControl(''),
+  });
 
   ngOnInit(): void {
   }
-  
+
   back() {
+
+  }
+
+  addDevice() {
 
   }
 }
--- src/app/modules/device/devices/adddevice/adddevice.module.ts
+++ src/app/modules/device/devices/adddevice/adddevice.module.ts
@@ -4,6 +4,12 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AdddeviceComponent } from './adddevice.component';
 
@@ -14,7 +20,13 @@ import { AdddeviceComponent } from './adddevice.component';
     MatMenuModule,
     MatIconModule,
     MatDividerModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    ReactiveFormsModule
   ],
   exports: [AdddeviceComponent]
 })
